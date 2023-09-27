a124d838fa8e78247e8bb4c221bea2c123876aa7
MateusRSdev <mateusRSdev@gmail.com>
2023-09-27 09:26:56 -0300

    parece que o jogo virou nao e mesmo

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) {
     return User::all();
 });
 
