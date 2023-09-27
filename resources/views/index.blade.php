a00ef188080120d4c7dcdd298ca5dea666afe146
MateusRSdev <mateusRSdev@gmail.com>
2023-09-27 07:49:22 -0300

    so pecçço que funcione

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sera um milagre ?</title>
-
-    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+    @vite("resources/js/app.js")
+    @vite("resources/sass/app.scss")
 </head>
 <body>
     <h1 class="display-1">EU TENHO A FORCA!!!!</h1>
