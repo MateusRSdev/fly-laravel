28bbcc8f1e68d39e5c835fa29d919c88ef7b0e93
MateusRSdev <mateusRSdev@gmail.com>
2023-09-27 07:43:42 -0300

    so pecçço que funcione

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if($this->app->environment('production')) {
+            dd("entrou");
             \URL::forceScheme('https');
         }
     }
