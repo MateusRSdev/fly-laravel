587dff5406c85f46971c8e9069910ecdabbe2873
MateusRSdev <mateusRSdev@gmail.com>
2023-09-28 10:40:14 -0300

    so pecçço que funcione

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,13 @@
-@extends('layout.layout-base')
-@section('content')
-<h1>teste</h1>
-    
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testvdsag</title>
+</head>
+<body>
+    aaaaaaaaaaaaa
+</body>
+</html>
 
