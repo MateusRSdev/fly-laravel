8413e9995d335506ff622031d9504471176b9f43
MateusRSdev <mateusRSdev@gmail.com>
2023-09-28 10:53:58 -0300

    so pecçço que funcione

--- resources/views/layout/layout-base.blade.php
+++ resources/views/layout/layout-base.blade.php
@@ -9,7 +9,33 @@
     @vite("resources/sass/app.scss")
 </head>
 <body>
-    <x-navbar/>
+    <nav class="navbar navbar-dark p-3 bg-dark d-flex justify-content-beetwen">
+        <a class="navbar-brand" href="#">
+            <img src="" width="30" height="30" class="d-inline-block align-top" alt="">
+            API de form's
+        </a>
+        
+        <ul class="navbar-nav d-flex flex-row">
+            <li class="nav-link mx-2">
+                asass
+            </li>
+            <li class="nav-link mx-2">
+                asass
+            </li>
+            <li class="nav-link mx-2">
+                asass
+            </li>
+            <li class="nav-link mx-2">
+                asass
+            </li>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-link">
+                <a href="" class="btn btn-warning">Login</a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
+    
 </body>
 </html>
